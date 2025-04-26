df8795ab6f56d028685e8451f912783974654111
Rafael Albuquerque <rafael.albuquerquev@gmail.com>
2025-04-26 09:03:07 -0300

    Add project files.

---
+++ ClubeDaLeitura.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ClubeDaLeitura.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------");
+            Console.WriteLine("CLUBE DA LEITURA");
+            Console.WriteLine("---------------------");
+            Console.WriteLine();
+
+        }
+    }
+}
