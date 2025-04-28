f78c0f3e285f84c563cefa81a45a20efe1e43934
Rafael Albuquerque <rafael.albuquerquev@gmail.com>
2025-04-28 15:15:13 -0300

    Implementação de repositórios

---
+++ ClubeDaLeitura.ConsoleApp/ModuloCliente/RepositorioCliente.cs
@@ -0,0 +1,6 @@
+﻿namespace ClubeDaLeitura.ConsoleApp.ModuloCliente
+{
+    public class RepositorioCliente
+    {
+    }
+}
---
+++ ClubeDaLeitura.ConsoleApp/ModuloRevista/RepositorioRevista.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubeDaLeitura.ConsoleApp.ModuloRevista
+{
+    internal class RepositorioRevista
+    {
+    }
+}
